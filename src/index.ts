6548152bd796b806d9339aa4a2a18a2374b60f43
remvst <remvst@gmail.com>
2023-12-10 21:29:27 -0500

    Polishhhhh

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import { promises as fs } from 'fs';
 import moment from 'moment';
 import { execShellCommand } from './utils';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
 
 const SPLITTER = 'yolobrolo';
 
 async function main() {
-    const generatedTs = '../shared/src/changelog.ts';
+    const argv = await yargs(hideBin(process.argv))
+        .options({
+            'outDir': { 
+                type: 'string', 
+                default: '.', 
+                alias: 'o', 
+                describe: 'Directory to generate the files into',
+            },
+            'packageJson': { 
+                type: 'string', 
+                default: './package.json', 
+                alias: 'p', 
+                describe: 'package.json file to use for the version',
+            }
+        })
+        .argv
+
+    const generatedTs = argv.outDir + '/changelog.ts';
+    const generatedJson = argv.outDir + '/changelog.json';
 
     try {
         await fs.rm(generatedTs, { 'recursive': true });
@@ -34,7 +54,7 @@ async function main() {
         }
     }
 
-    const packageJson = JSON.parse(await fs.readFile(__dirname + '/../../package.json', 'utf8'));
+    const packageJson = JSON.parse(await fs.readFile(argv.packageJson, 'utf8'));
 
     let generatedFileContent = '';
     generatedFileContent += 'import VERSION_JSON from "./changelog.json";\n\n';
@@ -60,7 +80,7 @@ async function main() {
         },
         'changelog': displayableLogs,
     };
-    await fs.writeFile('../shared/src/changelog.json', JSON.stringify(versionFileContent));
+    await fs.writeFile(generatedJson, JSON.stringify(versionFileContent));
 }
 
 main();
